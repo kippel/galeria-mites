898d2dd3656efecbfdda5871ace60c57aa48a49a
david <kippeld@gmail.com>
2011-03-11 17:41:35 +0100

    definit model

--- photos/models.py
+++ photos/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class Photos(models.Model):
   name = models.CharField(max_length=100)
+  description = models.TextField()
+  file_name = models.charField(max_length=100)
   tags = models.TextField()
+  ## posar com a fulltext els tags
