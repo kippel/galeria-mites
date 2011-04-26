b1a76e857f0d7c4fa2323d00ef4dac8fbfcde577
david <kippeld@gmail.com>
2011-04-26 18:11:48 +0200

    llicencia GNU

--- photos/models.py
+++ photos/models.py
@@ -1,14 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+#       models.py     
+#       
+#       Copyright 2011 David <kippeld@gmail.com>            
+#                      Arnau <sacaix@gmail.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#       
+# 
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.contrib import admin
 import uuid
 
-"""
-def get_file_path(instance, filename):
-  ext = filename.split('.')[-1]
-  filename = "%s.%s" % (uuid.uuid4(), ext)
-  return os.path.join('photos', filename)
-"""
 fs = FileSystemStorage()
 
 # Create your models here.
